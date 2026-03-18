5aa7a1ca7c274e1a776ea9294e3a90f6f2112025
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-03-18 14:53:13 +0100

    task 5

--- react_intro/task_2/dashboard/src/App.spec.js
+++ react_intro/task_2/dashboard/src/App.spec.js
@@ -24,3 +24,28 @@ test('renders img with alt text holberton logo', () => {
   const img = screen.getByAltText(/holberton logo/i);
   expect(img).toBeInTheDocument();
 });
+
+test('renders 2 input elements', () => {
+  render(<App />);
+  const inputs = screen.getAllByRole('textbox');
+  const passwordInput = document.querySelector('input[type="password"]');
+  expect(inputs.length + (passwordInput ? 1 : 0)).toBe(2);
+});
+
+test('renders label with text Email', () => {
+  render(<App />);
+  const emailLabel = screen.getByText(/email/i);
+  expect(emailLabel).toBeInTheDocument();
+});
+
+test('renders label with text Password', () => {
+  render(<App />);
+  const passwordLabel = screen.getByText(/password/i);
+  expect(passwordLabel).toBeInTheDocument();
+});
+
+test('renders a button with text OK', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /ok/i });
+  expect(button).toBeInTheDocument();
+});
