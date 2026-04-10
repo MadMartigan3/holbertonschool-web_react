225e19772b605cd903386612a484d35bfc659ea4
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-04-10 10:04:37 +0200

    correction pour rendu

--- react_props/task_0/dashboard/src/App/App.spec.js
+++ react_props/task_0/dashboard/src/App/App.spec.js
@@ -1,8 +1,8 @@
 import { render } from '@testing-library/react';
-import Header from './Header';
+import App from './App';
 
-describe('Header', () => {
+describe('App', () => {
   it('renders without crashing', () => {
-    render(<Header />);
+    render(<App />);
   });
 });
