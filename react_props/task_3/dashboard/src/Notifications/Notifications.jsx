267577fdcbbfb70251cbf92efa7e688e59be8925
MadMartigan3 <gabrielmilan3@hotmail.com>
2026-04-10 10:52:24 +0200

    task 4

---
+++ react_props/task_3/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,41 @@
+import closeIcon from '../assets/close-button.png';
+import NotificationItem from './NotificationItem';
+import './Notifications.css';
+
+function Notifications({ notifications = [] }) {
+  return (
+    <div className="notification-items">
+      <p>Here is the list of notifications</p>
+      <button
+        aria-label="Close"
+        style={{
+          position: 'absolute',
+          top: '5px',
+          right: '8px',
+          background: 'none',
+          border: 'none',
+          cursor: 'pointer',
+          fontSize: '14px',
+          color: '#333',
+          padding: '0',
+          lineHeight: '1',
+        }}
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img src={closeIcon} alt="close icon" style={{ width: '10px', height: '10px' }} />
+      </button>
+      <ul>
+        {notifications.map((notification) => (
+          <NotificationItem
+            key={notification.id}
+            type={notification.type}
+            html={notification.html}
+            value={notification.value}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
